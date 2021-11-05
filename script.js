91be1f395a45967adca5b3e4dc1967ccb47a8a63
whoguy24 <whoguy24@gmail.com>
2021-11-05 18:41:22 -0500

    Added deleteEmployee function and tested.

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 $(document).ready(onReady);
 
 let employees = [
+    /*
     {
         firstName: 'Warren',
         lastName: 'OBrien',
-        ID: 4324,
-        title: 'Software Intern',
-        salary: 10
+        ID: 5,
+        title: 'Developer',
+        salary: 2456
     }
+    */
 ];
 
 function onReady() {
     console.log('jQuery loaded successfully.');
     renderEmployeeTable(employees);
     $('#button-add-employee').on('click', createEmployee)
+    $('#employee-table-data').on('click', '.button-delete', deleteEmployee)
 }
 
 function renderEmployeeTable (employeeData) {
     $('#employee-table-data').empty();
     for (let employee of employeeData) {
         $('#employee-table-data').append(`
-            <tr>
+            <tr id="${employee.ID}">
                 <td>${employee.firstName}</td>
                 <td>${employee.lastName}</td>
                 <td>${employee.ID}</td>
                 <td>${employee.title}</td>
                 <td>${employee.salary}</td>
+                <td><button class="button-delete">Delete</button></td>
             </tr>
         `);
     }
@@ -55,4 +59,14 @@ function clearInputFields () {
     $('#field-ID').val(''),
     $('#field-title').val(''),
     $('#field-salary').val('')
+}
+
+function deleteEmployee() {
+    let employeeID = $(this).parent().parent().attr('id');
+    for (let employee of employees) {
+        if (employee.id = employeeID) {
+            employees.splice(employee)  
+        }
+    }
+    $(this).parent().parent().remove();  
 }
