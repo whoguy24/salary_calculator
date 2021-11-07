2abb44f450e656332f31accc45b51bb505ce6e48
whoguy24 <whoguy24@gmail.com>
2021-11-07 11:24:20 -0600

    Added logic to turn total monthly text red if max allowed salary is exceeded.

--- script.js
+++ script.js
@@ -4,23 +4,37 @@ let employees = [
     {
         firstName: 'Warren',
         lastName: 'OBrien',
-        ID: 5,
+        ID: 1645,
         title: 'Developer',
-        salary: 2456
+        salary: 5000
     },
     {
         firstName: 'Gary',
         lastName: 'Jones',
-        ID: 6,
+        ID: 2185,
         title: 'Developer',
-        salary: 2186
+        salary: 2000
     },
     {
         firstName: 'Marty',
         lastName: 'Brooks',
-        ID: 7,
+        ID: 2685,
         title: 'Developer',
-        salary: 1645
+        salary: 5000
+    },
+    {
+        firstName: 'John',
+        lastName: 'Sanders',
+        ID: 2184,
+        title: 'Developer',
+        salary: 5000
+    },
+    {
+        firstName: 'Lisa',
+        lastName: 'Morrison',
+        ID: 2198,
+        title: 'Developer',
+        salary: 2000
     }
 ];
 
@@ -31,9 +45,9 @@ function onReady() {
     $('#employee-table-data').on('click', '.button-delete', deleteEmployee)
 }
 
-function renderEmployeeTable (employeeData) {
+function renderEmployeeTable (employees) {
     $('#employee-table-data').empty();
-    for (let employee of employeeData) {
+    for (let employee of employees) {
         $('#employee-table-data').append(`
             <tr id="${employee.ID}">
                 <td>${employee.firstName}</td>
@@ -45,10 +59,14 @@ function renderEmployeeTable (employeeData) {
             </tr>
         `);
     }
-
     let totalMonthly = calculateTotalSalary();
     $('#total-monthly').text('');
     $('#total-monthly').text(totalMonthly);
+    if (totalMonthly >= 20000) {
+        $('#total-monthly').addClass('total-monthly-red')
+    } else {
+        $('#total-monthly').removeClass('total-monthly-red')
+    }
 }
 
 function retrieveInputData () {
@@ -63,20 +81,20 @@ function retrieveInputData () {
     return newEmployee;
 }
 
-function createEmployee () {
-    let newEmployee = retrieveInputData()
-    newEmployee.ID = Number(newEmployee.ID);
-    newEmployee.salary = Number(newEmployee.salary);
-    employees.push(newEmployee);
-    renderEmployeeTable(employees);
-}
-
 function clearInputFields () {
     $('#field-firstName').val(''),
     $('#field-lastName').val(''),
     $('#field-ID').val(''),
     $('#field-title').val(''),
     $('#field-salary').val('')
+}
+
+function createEmployee () {
+    let newEmployee = retrieveInputData()
+    newEmployee.ID = Number(newEmployee.ID);
+    newEmployee.salary = Number(newEmployee.salary);
+    employees.push(newEmployee);
+    renderEmployeeTable(employees);
 }
 
 function deleteEmployee() {
